ab12e35cfe3b50e166be84276e2d7b9d8cb7c4c8
Roman Pasichniy <roman.pasichniy@gmail.com>
2026-06-06 18:57:54 +0300

    release

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: your name</p>
+          <p>Developer: Roman Pasichniy</p>
           <p>
             Contact us:
-            <a href="mailto:student@notehub.app">student@notehub.app</a>
+            <a href="mailto:roman.pasichniy@gmail.com">roman.pasichniy@gmail.com</a>
           </p>
         </div>
       </div>
